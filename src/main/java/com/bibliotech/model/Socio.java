c69602c08bb2a1abf54b258dafa3220ba6aeb452
juancruz0912 <j.rupcic@alumno.um.edu.ar>
2026-04-27 12:58:59 -0300

    feat: Adding getters and setters

--- src/main/java/com/bibliotech/model/Socio.java
+++ src/main/java/com/bibliotech/model/Socio.java
@@ -15,5 +15,46 @@ public abstract class Socio {
         this.apellido = apellido;
         this.email = email;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public abstract int getLimiteLibros();
 }
